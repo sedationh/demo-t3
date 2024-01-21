695a6abccda34948ae268b68907931b36dc5e4a3
SedationH <sedationh@gmail.com>
2024-01-21 14:23:12 +0800

    Complete the database query and creation

---
+++ src/middleware.ts
@@ -0,0 +1,15 @@
+import { authMiddleware, withClerkMiddleware } from "@clerk/nextjs";
+import { NextRequest, NextResponse } from "next/server";
+
+// This example protects all routes including api/trpc routes
+// Please edit this to allow other routes to be public as needed.
+// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
+// export default authMiddleware({});
+
+export default withClerkMiddleware(() => {
+  NextResponse.next();
+});
+
+export const config = {
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,32 +1,19 @@
 import { z } from "zod";
-
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const postRouter = createTRPCRouter({
-  hello: publicProcedure
-    .input(z.object({ text: z.string() }))
-    .query(({ input }) => {
-      return {
-        greeting: `Hello ${input.text}`,
-      };
-    }),
+  getALl: publicProcedure.query(({ ctx }) => {
+    return ctx.db.post.findMany();
+  }),
 
   create: publicProcedure
-    .input(z.object({ name: z.string().min(1) }))
+    .input(z.object({ content: z.string(), authorId: z.string() }))
     .mutation(async ({ ctx, input }) => {
-      // simulate a slow db call
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-
       return ctx.db.post.create({
         data: {
-          name: input.name,
+          content: input.content,
+          authorId: input.authorId,
         },
       });
     }),
-
-  getLatest: publicProcedure.query(({ ctx }) => {
-    return ctx.db.post.findFirst({
-      orderBy: { createdAt: "desc" },
-    });
-  }),
 });
